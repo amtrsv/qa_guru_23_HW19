cc019eae3062b7cca17fd3df866bc3861d644c6c
Andrey Matrosov <amtrsv@yandex.ru>
2024-01-09 21:07:56 +0300

    added lowCase named of method

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GitHubTest extends TestBase {
     @Test
     @DisplayName("Проверяем заголовок страницы")
-    public void GitHubTitleTest() {
+    public void gitHubTitleTest() {
         assertThat(title()).contains("GitHub: Let’s build from here · GitHub");
 
     }
